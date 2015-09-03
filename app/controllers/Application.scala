db56144cde8a44a6e185a87cb0558cac3607fdb6
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2015-09-03 10:18:26 +0100

    creating merge conflict

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-  	// create merge conflict
+  	// Adding 2 create merge conflict
   	import play.api.libs.json._
   	 Ok(JsObject(
   	 	Seq(
