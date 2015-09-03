eb5d02d0761f02f0ddc3c0b344729614b81f5da5
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2015-09-03 09:18:17 +0100

    Trying to create a merge conflict

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
+  	// Adding a comment create merge conflict
   	import play.api.libs.json._
   	 Ok(JsObject(
   	 	Seq(
